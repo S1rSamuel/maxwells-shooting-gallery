435cd567ae2540146001681e891b7a2cccf63211
Sam Urbaniak <spurbaniak@email.neit.edu>
2024-11-09 15:37:18 -0500

    added controls

--- w5_Game_Start_Files/js/controls.js
+++ w5_Game_Start_Files/js/controls.js
@@ -1,10 +1,12 @@
 /*-------------------------------
 Booleans to store whether a specific button is pressed or not
  --------------------------------*/
-var w = false;
+//var w = false;
 var a = false;
-var s = false;
+//var s = false;
 var d = false;
+var shoot = false
+var startGame = false 
 
 /*---Key Press Code-----------*/
 
@@ -12,18 +14,22 @@ document.addEventListener(`keydown`, press);
 function press(e)
 {
     console.log(e.keyCode)
-    if(e.keyCode == 87){w = true}
-    if(e.keyCode == 83){s = true;}
+   // if(e.keyCode == 87){w = true}
+    //if(e.keyCode == 83){s = true;}
     if(e.keyCode == 65){a = true;}
     if(e.keyCode == 68){d = true;}
+    if(e.keyCode == 32){shoot = true;}
+    if(e.keyCode == 13){startGame = true;}
 }
 
 /*---Key Release Code-----------*/
 document.addEventListener(`keyup`, release);
 function release(e)
 {
-    if(e.keyCode == 87){w = false;}
-    if(e.keyCode == 83){s = false;}
+   // if(e.keyCode == 87){w = false;}
+   // if(e.keyCode == 83){s = false;}
     if(e.keyCode == 65){a = false;}
     if(e.keyCode == 68){d = false;}
+    if(e.keyCode == 32){shoot = false;}
+    if(e.keyCode == 13){StartGame = false;}
 }
